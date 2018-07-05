816d03b9bd63b7fa1f03eeb39b6fcb49ae4adb37
hrilev <hrilev@yandex.ru>
2018-07-05 18:25:30 +0300

    Убрано все лишнее из проекта Задача #5

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <microblog title="Микроблоги"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import microblog from '@/components/microblog.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    microblog
   }
 }
 </script>
