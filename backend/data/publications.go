a1556ea1fa5f88fdb8872f7cc38f183dbffb91a0
Alexandr Nikishin <San4a-97@mail.ru>
2018-07-06 04:07:47 +0300

    +1

--- backend/data/publications.go
+++ backend/data/publications.go
@@ -1,14 +1,14 @@
 package data
 
-//Publications структура для хранения публикации
-type Publications struct {
+//Publication структура для хранения публикации
+type Publication struct {
 	Namepub    string `json:"namepub"`
 	Time       string `json:"time"`
 	Publcation string `json:"publication"`
 }
 
 //PublicationList хранимый список публикаций
-var PublicationList = []Publications{Publications{
+var PublicationList = []Publication{Publication{
 	Namepub:    "Название",
 	Time:       "Время",
 	Publcation: "Публикация"}}
