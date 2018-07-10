583fbd7c4bd0c7666af53d5315c7c32e0eb83360
Alexandr Nikishin <San4a-97@mail.ru>
2018-07-10 21:50:49 +0300

    Добавлена работа с BD и исправлены tests Задача #15

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/ComputePractice2018/microblog/backend/data"
 
 	"github.com/ComputePractice2018/microblog/backend/server"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
-	//name := flag.String("name", "Александр", "имя для преветствия")
-	//flag.Parse()
+	connection := flag.String("connection", "microblog:SecretPassword@tcp(db:3306)/microblog", "mysql connection string")
+	flag.Parse()
 
-	publicationList := data.NewPublicationList()
-	profileList := data.NewProfileList()
-	commentList := data.NewCommentList()
+	publicationList, err := data.NewDBPublicationList(*connection)
+	profileList, err := data.NewDBProfileList(*connection)
+	commentList, err := data.NewDBCommentList(*connection)
+
+	defer profileList.CloseProfile()
+	defer publicationList.ClosePublication()
+	defer commentList.CloseComment()
+
+	if err != nil {
+		log.Fatalf("DB error: %+v", err)
+	}
 
 	router := server.NewRouter(publicationList, profileList, commentList)
 
